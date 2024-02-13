39fa33a725b0cc917ec1bba8da0d1405fd3a9a96
samlafontaine <sam@samlafontaine.com>
2024-02-13 23:49:37 +0100

    prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { GeistSans } from 'geist/font/sans';
+import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
   title: "Company Logos",
-  description: "Find & download over 250,000 company logos for free, in a simple, clean and beautiful interface.",
+  description:
+    "Find & download over 250,000 company logos for free, in a simple, clean and beautiful interface.",
   openGraph: {
-    title: 'Company Logos',
-    description: 'Find & download over 250,000 company logos for free, in a simple, clean and beautiful interface.',
-    url: 'https://copanylogos.xyz',
-    siteName: 'Company Logos',
-    locale: 'en_US',
-    type: 'website',
+    title: "Company Logos",
+    description:
+      "Find & download over 250,000 company logos for free, in a simple, clean and beautiful interface.",
+    url: "https://copanylogos.xyz",
+    siteName: "Company Logos",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -19,11 +21,11 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
-  }
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { useState, useRef } from 'react';
+import { Button } from "@/components/ui/button";
+import { useState, useRef } from "react";
 import {
   Table,
   TableBody,
@@ -11,7 +11,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -22,24 +22,32 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
-import { Input } from "@/components/ui/input"
-import { PlusIcon, BarsArrowUpIcon, ArrowPathIcon, ArrowDownOnSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
-
+} from "@/components/ui/alert-dialog";
+import { Input } from "@/components/ui/input";
+import {
+  PlusIcon,
+  BarsArrowUpIcon,
+  ArrowPathIcon,
+  ArrowDownOnSquareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 
 const CompanyLogoFinder = () => {
   const urlsInputRef = useRef<HTMLInputElement>(null);
-  const [companyUrls, setCompanyUrls] = useState<string[]>(['']);
+  const [companyUrls, setCompanyUrls] = useState<string[]>([""]);
 
   const generateUrls = (companyUrl: string) => {
     if (companyUrl) {
       const clearbitUrl = `https://logo.clearbit.com/${companyUrl}`;
       return `${clearbitUrl}?size=800&format=png`;
     }
-    return ''; // Return an empty string if company URL is not provided
+    return ""; // Return an empty string if company URL is not provided
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
     const { value } = event.target;
     const newCompanyUrls = [...companyUrls];
     newCompanyUrls[index] = value;
@@ -47,7 +55,7 @@ const CompanyLogoFinder = () => {
   };
 
   const addRow = () => {
-    setCompanyUrls([...companyUrls, '']);
+    setCompanyUrls([...companyUrls, ""]);
   };
 
   const handleDeleteRow = (index: number) => {
@@ -57,7 +65,9 @@ const CompanyLogoFinder = () => {
   };
 
   const handleBulkInput = () => {
-    const urlsInputElement = document.getElementById('urls') as HTMLInputElement;
+    const urlsInputElement = document.getElementById(
+      "urls",
+    ) as HTMLInputElement;
     if (urlsInputElement) {
       const urls = urlsInputElement.value;
       if (urls) {
@@ -74,7 +84,6 @@ const CompanyLogoFinder = () => {
     }
   };
   */
-  
 
   const reloadPage = () => {
     location.reload();
@@ -82,12 +91,12 @@ const CompanyLogoFinder = () => {
 
   const downloadImage = (imageUrl: string) => {
     fetch(imageUrl)
-      .then(response => response.blob())
-      .then(blob => {
+      .then((response) => response.blob())
+      .then((blob) => {
         const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
+        const a = document.createElement("a");
         a.href = url;
-        a.download = 'logo.png';
+        a.download = "logo.png";
         document.body.appendChild(a);
         a.click();
         a.remove();
@@ -100,108 +109,124 @@ const CompanyLogoFinder = () => {
         <div className="flex flex-col items-center justify-center w-full">
           <div className="text-center">
             <h1 className="text-3xl">Free Company Logo Finder</h1>
-            <p className="text-md">Find <span className='text-orange-500 underline'>any company's logo</span> and download it in 1 click.</p>
+            <p className="text-md">
+              Find{" "}
+              <span className="text-orange-500 underline">
+                any company's logo
+              </span>{" "}
+              and download it in 1 click.
+            </p>
           </div>
           <div className="flex items-center justify-center mt-6 w-full md:w-7/12">
-          <Table className="max-w-screen-lg table-auto">
-            <TableHeader>
-              <TableRow>
-                <TableHead>Company URL</TableHead>
-                <TableHead>Company Logo</TableHead>
-                <TableHead>Action</TableHead>
-                <TableHead></TableHead>
-              </TableRow>
-            </TableHeader>
-
-            <TableBody id="company-table">
-              {companyUrls.map((url, index) => (
-                <TableRow key={index} className='h-1'>
-                  <TableCell style={{ verticalAlign: 'middle' }}>
-                    <Input
-                      type="text"
-                      className="company-url form-control"
-                      style={{ width: 'auto' }}
-                      placeholder="e.g. apple.com"
-                      value={url}
-                      onChange={(e) => handleInputChange(e, index)}
-                    />
-                  </TableCell>
-                  <TableCell style={{ verticalAlign: 'middle' }}>
-                    <img
-                      className="max-w-16"
-                      src={generateUrls(url)}
-                      alt=""
-                      crossOrigin="anonymous"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <Button
-                      variant="outline"
-                      size="sm"
-                      onClick={() => downloadImage(generateUrls(url))}
-                    >
-                      <ArrowDownOnSquareIcon className='w-4 h-4 mr-1'/>
-                      Download
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button
-                      variant="ghost"
-                      size="sm"
-                      className='hover:bg-red-100'
-                      onClick={() => handleDeleteRow(index)}
-                    >
-                      <TrashIcon className='w-4 h-4 stroke-red-500'/>
-                    </Button>
-                  </TableCell>
+            <Table className="max-w-screen-lg table-auto">
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Company URL</TableHead>
+                  <TableHead>Company Logo</TableHead>
+                  <TableHead>Action</TableHead>
+                  <TableHead></TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+
+              <TableBody id="company-table">
+                {companyUrls.map((url, index) => (
+                  <TableRow key={index} className="h-1">
+                    <TableCell style={{ verticalAlign: "middle" }}>
+                      <Input
+                        type="text"
+                        className="company-url form-control"
+                        style={{ width: "auto" }}
+                        placeholder="e.g. apple.com"
+                        value={url}
+                        onChange={(e) => handleInputChange(e, index)}
+                      />
+                    </TableCell>
+                    <TableCell style={{ verticalAlign: "middle" }}>
+                      <img
+                        className="max-w-16"
+                        src={generateUrls(url)}
+                        alt=""
+                        crossOrigin="anonymous"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => downloadImage(generateUrls(url))}
+                      >
+                        <ArrowDownOnSquareIcon className="w-4 h-4 mr-1" />
+                        Download
+                      </Button>
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        variant="ghost"
+                        size="sm"
+                        className="hover:bg-red-100"
+                        onClick={() => handleDeleteRow(index)}
+                      >
+                        <TrashIcon className="w-4 h-4 stroke-red-500" />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </div>
           <br />
           <div className="flex flex-row gap-1.5 mb-2">
-          <Button 
-            className='gap-1'
-            size="sm" 
-            onClick={addRow}>
-            <PlusIcon className="w-4 h-4"/>
-            <span>Add Row</span>
-          </Button>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button
-                className='gap-1'
-                size="sm"
-              >
-              <BarsArrowUpIcon className="w-4 h-4"/>
-              <span>Add Bulk</span>
-              </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Add bulk</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Enter a comma-separated list of company URLs:
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <Input type="text" id="urls" ref={urlsInputRef} />
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction onClick={handleBulkInput}>Submit</AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-          <Button 
-            className='gap-1'
-            size="sm" 
-            variant="outline"
-            onClick={reloadPage}>
-            <ArrowPathIcon className="w-4 h-4"/>
-            <span>Start Over</span>
-          </Button>
+            <Button className="gap-1" size="sm" onClick={addRow}>
+              <PlusIcon className="w-4 h-4" />
+              <span>Add Row</span>
+            </Button>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button className="gap-1" size="sm">
+                  <BarsArrowUpIcon className="w-4 h-4" />
+                  <span>Add Bulk</span>
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Add bulk</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Enter a comma-separated list of company URLs:
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <Input type="text" id="urls" ref={urlsInputRef} />
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleBulkInput}>
+                    Submit
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+            <Button
+              className="gap-1"
+              size="sm"
+              variant="outline"
+              onClick={reloadPage}
+            >
+              <ArrowPathIcon className="w-4 h-4" />
+              <span>Start Over</span>
+            </Button>
           </div>
-          <p className="text-xs mt-1">Built by <a href="https://twitter.com/samlafontaine_" className='font-medium underline'>Sam</a>. Logos provided by <a href="https://clearbit.com" className='font-medium underline'>Clearbit</a>.</p>
+          <p className="text-xs mt-1">
+            Built by{" "}
+            <a
+              href="https://twitter.com/samlafontaine_"
+              className="font-medium underline"
+            >
+              Sam
+            </a>
+            . Logos provided by{" "}
+            <a href="https://clearbit.com" className="font-medium underline">
+              Clearbit
+            </a>
+            .
+          </p>
         </div>
       </div>
     </div>
